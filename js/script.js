a132943ed63dbcc0a727b5b13c27c5894ed57e64
Ricevind <ricevind@gmail.com>
2016-11-07 09:04:56 +0100

    menu and scroll animation

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+$document = $(document);
+var fixedNav = $("nav").clone().insertAfter('nav');
+fixedNav.addClass('sticky').hide();
+
+	function navScroll() {
+		var nav = $("nav");
+		var navHeight = nav.outerHeight(true);
+		var mainHeader = $('#mainHeader');
+		var navPosition = mainHeader.outerHeight(true) + navHeight;
+		var slideOptions = {duration:400, easing:'swing'};
+
+		if ($document.scrollTop() > navPosition ) {
+			fixedNav.slideDown(slideOptions);
+		}
+		else if ($document.scrollTop() < mainHeader.outerHeight(true) + navHeight){
+			fixedNav.slideUp(slideOptions);
+		}
+	}
+	$document.on("scroll", navScroll);
+	navScroll();
+//
+function elemPosition(selector){
+	return $(selector).position().top;
+	}
+$('a').click(function(e){
+	e.preventDefault();
+	var id = $(this).attr('href');
+	var position = elemPosition(id);
+	$('html, body').stop().animate({scrollTop:position - $('nav.sticky').outerHeight()}, Math.abs($document.scrollTop()-position), 'easeInOutQuad');
+	});
+});
